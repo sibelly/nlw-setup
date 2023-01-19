fa1584e05b337e2835c0f6033065f9e52a3a2df2
sibelly <sibellychan@hotmail.com>
2023-01-19 00:27:38 -0400

    feat: logic to fill the rest of the days for the table habits

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -9,6 +10,11 @@ const weekDays = [
   'S',
   'S'
 ]
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize =  18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
@@ -30,22 +36,23 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {
+          summaryDates.map(date => {
+            return <HabitDay key={date.toString()}/>
+          })
+        }
+        
+        {
+          amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            )
+          })
+        }
+
       </div>
 
     </div>

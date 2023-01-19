4d52083a7ba5c530f916b9e4ad62c119577bf90e
sibelly <sibellychan@hotmail.com>
2023-01-19 02:05:29 -0400

    feat: finishes home with summary dates

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
@@ -12,6 +13,9 @@ const weekDays = [
   'S',
   'S',
 ]
+const dateFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - dateFromYearStart.length
 
 export function Home() {
   return (
@@ -30,7 +34,33 @@ export function Home() {
           ))
         }
       </View>
-      <HabitDay />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        <View className="flex-row flex-wrap">
+          <HabitDay />
+          {
+            dateFromYearStart.map(date => (
+              <HabitDay
+                key={date.toISOString()}
+              />
+            ))
+          }
+          {
+            amountOfDaysToFill > 0 && Array
+            .from({length: amountOfDaysToFill})
+            .map((_,i) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{width: DAY_SIZE, height: DAY_SIZE}}/>
+            ))
+          }
+        </View>
+
+      </ScrollView>
+
     </View>
   )
 }

8d965b0257b76d209b6a8f019f91b547386b481f
sibelly <sibellychan@hotmail.com>
2023-01-19 00:06:17 -0400

    feat: habits table and components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
-import { Habit } from "./components/Habit"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
 import './styles/global.css'
 
-function App() {
+export function App() {
 
   return (
-    <>
-      <Habit completed={3}/>
-      <Habit completed={6}/>
-      <Habit completed={8}/>
-      <Habit completed={12}/>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+        <Header />
+        <SummaryTable />
+      </div>
+
+    </div>
   )
 }
 

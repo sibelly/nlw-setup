61103bdaabf1c3574343901e67b41c9500fdb528
sibelly <sibellychan@hotmail.com>
2023-01-23 00:20:05 -0400

    feat: new habit screen, progress bar, checkbox back button components

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from "react-native";
-
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+import { useNavigation } from "@react-navigation/native";
+
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
@@ -18,6 +19,7 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - dateFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation();
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -45,6 +47,7 @@ export function Home() {
             dateFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }

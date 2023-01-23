bacd59d468fcd7dc1fd196523baca73d70403110
sibelly <sibellychan@hotmail.com>
2023-01-23 02:03:53 -0400

    feat: start integratin with backend

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
   'Sunday',
@@ -19,6 +20,18 @@ export function NewHabitForm() {
   function createNewHabit(event: FormEvent) {
     event.preventDefault()
     console.log('create ', title)
+    if (!title || weekDays.length == 0) {
+      return
+    }
+
+    api.post('habits',{
+      title,
+      weekDays
+    })
+    setTitle('')
+    setWeekDays([])
+
+    alert('habit created!')
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -46,6 +59,7 @@ export function NewHabitForm() {
         placeholder="ex.: Exercises, sleep weel, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 txt-white placeholder:text-zinc-400"
         autoFocus
+        value={title}
         onChange={event => setTitle(event.target.value)}
       />
 
@@ -60,6 +74,7 @@ export function NewHabitForm() {
               <Checkbox.Root
                 key={weekDay}
                 className='flex items-center gap-3 group'
+                checked={weekDays.includes(index)}
                 onCheckedChange={() => handleToggleWeekDay(index)}
               >
                 <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
